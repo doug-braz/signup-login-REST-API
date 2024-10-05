525889206ec9f04887f6985962ec74427c9ef6a4
doug-braz <douglas.mateus29@gmail.com>
2024-10-05 16:03:39 -0300

    Basic setup of the Django Project and creation of the signup app

---
+++ signup/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class SignUp(models.model):
+    username = models.CharField(max_length = 30, null=False, blank=False, unique=True)
+    password = models.CharField(max_length = 50, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    first_name = models.CharField(max_length = 50, null=False, blank=False)
+    last_name = models.CharField(max_length = 50, null=False, blank=False)
+
+    def __str__(self):
+        return self.username
+
+# Create your models here.
