805f1ca15679cec1c36bcab453eeae363b98b477
doug-braz <douglas.mateus29@gmail.com>
2024-10-06 11:21:08 -0300

    Created the route for the sign up page and a placeholder page for it

--- signup/models.py
+++ signup/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class SignUp(models.model):
+class SignUp(models.Model):
     username = models.CharField(max_length = 30, null=False, blank=False, unique=True)
     password = models.CharField(max_length = 50, null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
--- signup/views.py
+++ signup/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def SignUp(request):
+    return render(request, 'signup/signup.html')
